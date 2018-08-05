6ea6942c92390fd638e69c6d8bc9049263aa60aa
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-05 13:40:00 -0700

    refactored job_actions to use github jobs api instead of indeed

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -3,34 +3,30 @@ import reverseGeocode from "latlng-to-zip";
 import qs from "qs";
 
 import { FETCH_JOBS } from "./types";
-import JOB_DATA from "./IndeedJobData.json";
 
-const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
+const GITHUB_BASE_URL = "https://jobs.github.com/positions.json?";
 
-const JOB_QUERY_PARAMS = {
-  publisher: "4201738803816157",
-  format: "json",
-  v: "2",
-  latlong: 1,
-  radius: 10,
-  q: "javascript"
-};
+export const fetchJobs = ({
+  longitudeDelta,
+  latitudeDelta,
+  longitude,
+  latitude
+}) => {
+  return async dispatch => {
+    try {
+      const url = `${GITHUB_BASE_URL}lat=${latitude}&long=${longitude}`;
 
-const buildJobsUrl = zip => {
-  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
-  return `${JOB_ROOT_URL}${query}`;
-};
+      let { data } = await axios.get(url);
+      console.log("====================================");
+      console.log("this is the data", data);
+      console.log("====================================");
 
-export const fetchJobs = (region, callback) => async dispatch => {
-  try {
-    //let zip = await reverseGeoCode(region);
-    //const url = buildJobsUrl(zip);
-    //let { data } = await axios.get(url);
-    const data = JOB_DATA;
-    console.log(data);
-    dispatch({ type: FETCH_JOBS, payload: data });
-    callback();
-  } catch (e) {
-    console.log(e);
-  }
+      dispatch({
+        type: FETCH_JOBS,
+        payload: data
+      });
+    } catch (err) {
+      console.log("Something went wrong... ", err);
+    }
+  };
 };
