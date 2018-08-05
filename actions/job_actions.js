d03e390c9f48d9da9c1675889c665fc0d1e00c30
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-05 03:02:43 -0700

    query string for indeed api is formed dynamically

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -4,14 +4,21 @@ import qs from "qs";
 
 import { FETCH_JOBS } from "./types";
 
+const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
+
 const JOB_QUERY_PARAMS = {
-  publisher: '',
-  format: 'json',
-  v: '2',
+  publisher: "4201738803816157",
+  format: "json",
+  v: "2",
   latlong: 1,
   radius: 10,
-  q: 'javascript'
-}
+  q: "javascript"
+};
+
+const buildJobsUrl = zip => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zip });
+  return `${JOB_ROOT_URL}${query}`;
+};
 
 export const fetchJobs = region => async dispatch => {
   try {
