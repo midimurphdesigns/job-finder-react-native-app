89199c4d93e8850407c21714c62941a928abf4c3
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-04 21:56:57 -0700

    added onwards button to welcome screen going to auth screen

--- components/Slides.js
+++ components/Slides.js
@@ -1,21 +1,37 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View, ScrollView, Dimensions } from "react-native";
+import { Button } from "react-native-elements";
 
 const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default class Slides extends Component {
+  renderLastSlide(index) {
+    if (index === this.props.data.length - 1) {
+      return (
+        <Button
+          title="Onwards!"
+          raised
+          buttonStyle={styles.buttonStyle}
+          onPress={this.props.onComplete}
+        />
+      );
+    }
+  }
+
   renderSlides() {
-    return this.props.data.map(slide => {
+    return this.props.data.map((slide, index) => {
       return (
         <View
           key={slide.text}
           style={[styles.slideStyle, { backgroundColor: slide.color }]}
         >
           <Text style={styles.textStyle}>{slide.text}</Text>
+          {this.renderLastSlide(index)}
         </View>
       );
     });
   }
+
   render() {
     return (
       <ScrollView horizontal pagingEnabled style={{ flex: 1 }}>
@@ -33,8 +49,12 @@ const styles = StyleSheet.create({
     width: SCREEN_WIDTH
   },
   textStyle: {
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 30,
-    color: 'white'
+    color: "white",
+    marginBottom: 15
+  },
+  buttonStyle: {
+    backgroundColor: "#0288D1"
   }
 });
