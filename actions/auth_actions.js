3dec6f646ab3a6e551afdd8e085623c3b6c873cf
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-04 23:12:54 -0700

    completed facebook login actions

--- actions/auth_actions.js
+++ actions/auth_actions.js
@@ -28,4 +28,7 @@ const doFacebookLogin = async dispatch => {
   if (type === "cancel") {
     return dispatch({ type: FACEBOOK_LOGIN_FAIL });
   }
+
+  await AsyncStorage.setItem("fb_token", token);
+  dispatch({ type: FACEBOOK_LOGIN_SUCCESS, payload: token})
 };
