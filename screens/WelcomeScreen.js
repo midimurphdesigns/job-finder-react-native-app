a8b530e187f70613bd270d05cd9a8502f842e0a2
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-04 21:16:34 -0700

    linked in slides component

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,16 +1,11 @@
 import React, { Component } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import Slides from "../components/Slides";
 
 export default class WelcomeScreen extends Component {
   render() {
     return (
-      <View>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-        <Text> WelcomeScreen </Text>
-      </View>
+      <Slides />
     );
   }
 }
