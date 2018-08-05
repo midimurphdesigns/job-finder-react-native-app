bab2c8cb85cb26aae57a798e6cc6cd138476bfb5
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-04 21:15:04 -0700

    initialized slides component

---
+++ components/Slides.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+
+export default class Slides extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Slides </Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
