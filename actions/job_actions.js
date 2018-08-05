4cf302cf390950478c593748677f575440a59433
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-05 02:56:59 -0700

    added qs and job query params object

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,8 +1,22 @@
 import axios from "axios";
-import reverseGeocode from 'latlng-to-zip';
+import reverseGeocode from "latlng-to-zip";
+import qs from "qs";
 
 import { FETCH_JOBS } from "./types";
 
-export const fetchJobs = (region) => async dispatch => {
+const JOB_QUERY_PARAMS = {
+  publisher: '',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+}
 
+export const fetchJobs = region => async dispatch => {
+  try {
+    let zip = await reverseGeocode({ region });
+  } catch (e) {
+    console.error(e);
+  }
 };
