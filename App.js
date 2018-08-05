62d077bd0015f57664db4afd484bcfc35411a5ac
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-04 22:23:23 -0700

    implemented redux

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createBottomTabNavigator, createStackNavigator } from "react-navigation";
+import {
+  createBottomTabNavigator,
+  createStackNavigator
+} from "react-navigation";
+import { Provider } from "react-redux";
 
+import store from "./store";
 import AuthScreen from "./screens/AuthScreen";
 import WelcomeScreen from "./screens/WelcomeScreen";
-import MapScreen from './screens/MapScreen'
-import DeckScreen from './screens/DeckScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import ReviewScreen from './screens/ReviewScreen';
+import MapScreen from "./screens/MapScreen";
+import DeckScreen from "./screens/DeckScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ReviewScreen from "./screens/ReviewScreen";
 
 export default class App extends React.Component {
   render() {
@@ -29,7 +34,9 @@ export default class App extends React.Component {
     });
 
     return (
+      <Provider store={store}>
         <MainNavigator />
+      </Provider>
     );
   }
 }
