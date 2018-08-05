61ba1077138a706c4740cbe6bbd5bfa203ccf256
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-05 13:31:43 -0700

    work around indeed api key not working with static json data

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -3,6 +3,7 @@ import reverseGeocode from "latlng-to-zip";
 import qs from "qs";
 
 import { FETCH_JOBS } from "./types";
+import JOB_DATA from "./IndeedJobData.json";
 
 const JOB_ROOT_URL = "http://api.indeed.com/ads/apisearch?";
 
@@ -20,16 +21,16 @@ const buildJobsUrl = zip => {
   return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = region => async dispatch => {
+export const fetchJobs = (region, callback) => async dispatch => {
   try {
-    let zip = await reverseGeocode({ region });
-    const url = buildJobsUrl(zip);
-    let { data } = await axios.get(url);
+    //let zip = await reverseGeoCode(region);
+    //const url = buildJobsUrl(zip);
+    //let { data } = await axios.get(url);
+    const data = JOB_DATA;
+    console.log(data);
     dispatch({ type: FETCH_JOBS, payload: data });
-    console.log('====================================');
-    console.log('this is the data', data);
-    console.log('====================================');
+    callback();
   } catch (e) {
-    console.error(e);
+    console.log(e);
   }
 };
