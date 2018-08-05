aade6c5a093c62d336e19d8087e3203122c09d4b
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-04 19:55:46 -0700

    added a tab navigator and a nested tab navigator

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator, StackNavigator } from "react-navigation";
+
+import AuthScreen from "./screens/AuthScreen";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = createBottomTabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: createBottomTabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen }
+        })
+      }
+    });
+
     return (
-      <View style={styles.container}>
-        <Text>Hi Kevin!</Text>
-      </View>
+        <MainNavigator />
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    // flex: 1,
+    // backgroundColor: "#fff",
+    // alignItems: "flex-end",
+    // justifyContent: "center"
+  }
 });
