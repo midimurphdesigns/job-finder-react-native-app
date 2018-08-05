bfafb77b01e0518f11c239c4ecaa7cc0d8d2387b
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-05 01:46:57 -0700

    cleaned up auth screen display text

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -6,25 +6,31 @@ import * as actions from "../actions";
 class AuthScreen extends Component {
   componentDidMount = () => {
     this.props.facebookLogin();
-    AsyncStorage.removeItem("fb_token");
+    this.onAuthComplete(this.props);
   };
 
+  componentWillReceiveProps(nextProps) {
+    this.onAuthComplete(nextProps);
+  }
+
+  onAuthComplete(props) {
+    if (props.token) {
+      this.props.navigation.navigate("map");
+    }
+  }
+
   render() {
-    return (
-      <View>
-        <Text> AuthScreen </Text>
-        <Text> AuthScreen </Text>
-        <Text> AuthScreen </Text>
-        <Text> AuthScreen </Text>
-        <Text> AuthScreen </Text>
-      </View>
-    );
+    return <View />;
   }
 }
 
 const styles = StyleSheet.create({});
 
+function mapStateToProps({ auth }) {
+  return { token: auth.token };
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   actions
 )(AuthScreen);
