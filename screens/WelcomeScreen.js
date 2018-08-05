a78754bf6561ff979d8e81a0e5356098888d32e1
midimurphdesigns <midimurphdesigns@gmail.com>
2018-08-05 02:01:44 -0700

    welcome screen only shows first time you load app

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, AsyncStorage } from "react-native";
 import Slides from "../components/Slides";
 import _ from "lodash";
+import { AppLoading } from "expo";
 
 const SLIDE_DATA = [
   { text: "Welcome to JobApp", color: "#03A9F4" },
@@ -12,11 +13,26 @@ const SLIDE_DATA = [
 export default class WelcomeScreen extends Component {
   state = { token: null };
 
+  async componentWillMount() {
+    let token = await AsyncStorage.getItem("fb_token");
+
+    if (token) {
+      this.props.navigation.navigate("map");
+      this.setState({ token: false });
+    } else {
+      this.setState({ token: false });
+    }
+  }
+
   onSlidesComplete = () => {
     this.props.navigation.navigate("auth");
   };
 
   render() {
+    if (_.isNull(this.state.token)) {
+      return <AppLoading />;
+    }
+
     return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
   }
 }
